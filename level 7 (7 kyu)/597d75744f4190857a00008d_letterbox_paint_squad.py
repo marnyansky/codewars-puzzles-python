03b4c19fbd9b15effccc87a4c5229f539bdf5096
Marnyansky <marnyansky@gmail.com>
2021-05-21 04:46:45 +0300

    nested loop, pre-filled list, execution time: default_timer

--- level 7 (7 kyu)/597d75744f4190857a00008d_letterbox_paint_squad.py
+++ level 7 (7 kyu)/597d75744f4190857a00008d_letterbox_paint_squad.py
@@ -1,4 +1,4 @@
-'''
+"""
 Story:
 You and a group of friends are earning some extra money in the school holidays by re-painting the numbers on people's letterboxes for a small fee.
 Since there are 10 of you in the group each person just concentrates on painting one digit! For example, somebody will paint only the 1's, somebody else will paint only the 2's and so on...
@@ -7,7 +7,9 @@ To avoid any fights you need to distribute the money fairly. That's where this K
 
 Task:
 Given the start and end letterbox numbers, write a method to return the frequency of all 10 digits painted.
-'''
+"""
+
+from timeit import default_timer
 
 
 def paint_letterboxes(start, finish):
@@ -18,7 +20,7 @@ def paint_letterboxes(start, finish):
     return lst
 
 
-def paint_letterboxes_alt(start, finish):
+def paint_letterboxes_alt(start, finish):  # more efficient
     lst = [0] * 10
     for number in range(start, finish + 1):
         for str_digit in str(number):
@@ -27,5 +29,20 @@ def paint_letterboxes_alt(start, finish):
 
 
 # test of task (kata) https://www.codewars.com/kata/597d75744f4190857a00008d
+time_s = default_timer()
+
 print(paint_letterboxes(125, 132))  # [1,9,6,3,0,1,1,1,1,1]
+
+time_f = default_timer()
+execution_time_us = (time_f - time_s) * 1000000
+print(f"Execution time: {execution_time_us:.2f} microseconds")
+
+print('----')
+
+time_s = default_timer()
+
 print(paint_letterboxes_alt(125, 132))  # [1,9,6,3,0,1,1,1,1,1]
+
+time_f = default_timer()
+execution_time_us = (time_f - time_s) * 1000000
+print(f"Execution time: {execution_time_us:.2f} microseconds")
